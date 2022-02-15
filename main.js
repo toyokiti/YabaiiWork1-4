6f6bb021f64978186c80227101c595ce4fff9da7
toyoda.akihiro <akiakiakihiro0525@icloud.com>
2022-02-15 10:31:39 +0900

    2回目提出

--- main.js
+++ main.js
@@ -2,17 +2,17 @@
 
 // タスクを管理する配列
 const arrTask = [
-    {content: '机を片付ける.' , genre: '掃除'},
-    {content: '牛乳を買う.' , genre: '買い物'},
-    {content: '散歩する.' , genre: '運動'}    
+    {name: '机を片付ける.' , genre: '掃除'},
+    {name: '牛乳を買う.' , genre: '買い物'},
+    {name: '散歩する.' , genre: '運動'}    
     ]; 
 
-const inputTask = (taskContent, taskGenre) => {
-    let arr_tmp = {}; //タスクとジャンルを管理するオブジェクトの宣言
-    arr_tmp.content = taskContent;
-    arr_tmp.genre = taskGenre;
-    arrTask.push(arr_tmp);    
-
+const inputTask = (name, genre) => {
+    const task = {
+        name:  name,
+        genre:  genre,
+    }; 
+    arrTask.push(task);    
     showtask(); 
     alert('新しいタスクを追加しました。');
 }
@@ -22,16 +22,16 @@ const showtask = () => {
     console.log('現在持っているタスクの一覧');
     console.log('=============================');
     arrTask.forEach((value, index) => {
-        console.log(`${index} : [内容]${value.content} [ジャンル]${value.genre}`);
+        console.log(`${index} : [内容]${value.name} [ジャンル]${value.genre}`);
     })
 }
 
 showtask(); //最初に入っているタスクの表示
-let newTaskContent = prompt('タスクを入力してください');
+let newTaskName = prompt('タスクを入力してください');
 let newTaskGenre = prompt('ジャンルを入力してください');   
 
-if (newTaskContent !== null || newTaskGenre !== null ) { //タスクの追加(キャンセルをクリックした場合は無視)
-    inputTask(newTaskContent, newTaskGenre);
+if (newTaskName !== null || newTaskGenre !== null ) { //タスクの追加(キャンセルをクリックした場合は無視)
+    inputTask(newTaskName, newTaskGenre);
 } else {
     alert('タスクとジャンルの入力時にキャンセルがクリックされました。');
 }
